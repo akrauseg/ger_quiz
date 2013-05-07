57484c26f53379fb341f0cac1099d2f5582fad69
akrauseg <elvenhaven_mirkwood@hotmail.com>
2013-05-06 22:02:23 -0400

    Added ger->eng, eng->ger, and random direction quizing Need to look into UI for python

--- ger_quiz.py
+++ ger_quiz.py
@@ -6,7 +6,7 @@ number_of_db=1
 
 rounds=int(input('Enter number of rounds:'))
 print('Number of rounds:',rounds)
-
+trans_type=int(input('Enter translation type:\n\nGerman -> English [1]\nEnglish -> German [2]\nRandom [3]\n'))
 if(rounds<=0):
 	print("Invalid number of rounds...exiting")
 	exit()
@@ -29,7 +29,7 @@ def german(r):
 
 score=0
 #compares provided correct answer with guess
-def check_correct(correct,translations):
+def check_correct(correct,translation):
 	#read line also includes newline-> prolly a nicer way
 	if(str(correct.lower()+'\n') == str(translation).lower()):
 		print("correct")
@@ -67,24 +67,65 @@ quiz_t=get_list()
 print("this is the quiz list: ",quiz_t)
 r=1; #counter for rounds
 
-for i in quiz_t:
-	if(r>rounds):
-		break
-	direction=bool(random.getrandbits(1))
-	print("Round: ",r)
-	print("---------------------")
-	if(direction): #direction is true so ger->eng
+def random_trans():
+	global r
+	for i in quiz_t:
+		if(r>rounds):
+			break
+		direction=bool(random.getrandbits(1))
+		print("Round: ",r)
+		print("---------------------")
+		if(direction): #direction is true so ger->eng
+			print (german(i))
+			translation=sys.stdin.readline()
+			correct=english(i)
+			check_correct(correct,translation)
+		else: #direction is false so eng->ger
+			print(english(i))	
+			translation=sys.stdin.readline()
+			correct=german(i)
+			check_correct(correct,translation)
+		r=r+1
+		print("\n")
+
+def ger_to_eng():
+	global r
+	for i in quiz_t:
+		if(r>rounds):
+			break
+		print("Round: ",r)
+		print("---------------------")
 		print (german(i))
 		translation=sys.stdin.readline()
 		correct=english(i)
 		check_correct(correct,translation)
-	else: #direction is false so eng->ger
-		print(english(i))	
+		r=r+1
+		print("\n")
+
+def eng_to_ger():
+	global r
+	for i in quiz_t:
+		if(r>rounds):
+			break
+		print("Round: ",r)
+		print("---------------------")
+		print (english(i))
 		translation=sys.stdin.readline()
 		correct=german(i)
 		check_correct(correct,translation)
-	r=r+1
-	print("\n")
+		r=r+1
+		print("\n")
+
+if(trans_type==1):
+	ger_to_eng()
+elif(trans_type==2):
+	eng_to_ger()
+elif(trans_type==3):
+	random_trans()
+else:
+	print('You have entered an invalid translation direction...exiting')
+	exit()
+
 
 
 conn.close()
